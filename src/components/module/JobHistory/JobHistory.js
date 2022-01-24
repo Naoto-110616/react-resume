83bc5535ddaf6d4389b6fb7bfc7a9943645fc307
Naoto-110616 <siodeitadaku@gmail.com>
2022-01-24 23:55:55 +0900

    fix sentence

--- src/components/module/JobHistory/JobHistory.js
+++ src/components/module/JobHistory/JobHistory.js
@@ -22,7 +22,7 @@ const JobHistory = (props) => {
 	);
 	return (
 		<div className={classes["job-history"]}>
-			{!props.cv && <h2>職歴</h2>}
+			{!props.cv && <h2>履歴書</h2>}
 			{props.cv && <h2>職務経歴書</h2>}
 			{individual}
 			<h2>学歴</h2>
